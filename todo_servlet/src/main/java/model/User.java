2398eae6b67931e82534d9850a08ef7a51a68ca0
azu-wakinoza <wakinoza@gmail.com>
2025-09-29 09:45:40 +0900

    CheckStyleの警告部分を修正

--- todo_servlet/src/main/java/model/User.java
+++ todo_servlet/src/main/java/model/User.java
@@ -1,29 +1,35 @@
 package model;
+
 import java.io.Serializable;
 
-/**.
- * ユーザー情報を保持するクラス
+/**
+ * . ユーザー情報を保持するクラス
  */
 public class User implements Serializable {
-	/**. ユーザー名　*/
-	private String name;
-	
-	/**. パスワード　*/
-	private String pass;
-	
-	/**.
-	 * コンストラクタ
-	 */
-	public User() { }
-	public User(String name, String pass) {
-		this.name = name;
-		this.pass = pass;
-	}
-	
-	/**.
-	 * getterメソッド
-	 */
-	public String getName() { return this.name; }
-	public String getPass() { return this.pass; }
+  /** . ユーザー名 */
+  private String name;
 
+  /** . パスワード */
+  private String pass;
+
+  /**
+   * . コンストラクタ
+   */
+  public User() {}
+
+  public User(String name, String pass) {
+    this.name = name;
+    this.pass = pass;
+  }
+
+  /**
+   * . getterメソッド
+   */
+  public String getName() {
+    return this.name;
+  }
+
+  public String getPass() {
+    return this.pass;
+  }
 }
