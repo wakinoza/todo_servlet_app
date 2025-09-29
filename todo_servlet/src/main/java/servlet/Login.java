84b4e37b4e4d5c7aed53d973c70c149634809079
azu-wakinoza <wakinoza@gmail.com>
2025-09-29 10:05:42 +0900

    checkStyleの警告部分を修正

--- todo_servlet/src/main/java/servlet/Login.java
+++ todo_servlet/src/main/java/servlet/Login.java
@@ -1,7 +1,8 @@
 package servlet;
 
-import java.io.IOException;
 
+
+import java.io.IOException;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -13,29 +14,29 @@ import model.LoginLogic;
 import model.User;
 
 /**
- * ログイン処理するサーブレットクラス
+ * . ログイン処理するサーブレットクラス
  */
 @WebServlet("/Login")
 public class Login extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
-		String name = request.getParameter("name");
-		String pass = request.getParameter("pass");
-		User user = new User(name, pass);
-		LoginLogic loginLogic = new LoginLogic();
-		boolean isLogin = loginLogic.execute(user);
-		
-		if (isLogin) {
-			HttpSession session = request.getSession();
-			session.setAttribute("loginUser", user);
-		}
-		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/loginResult.jsp");
-		dispatcher.forward(request, response);
-	}
+  /**
+   * . @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    request.setCharacterEncoding("UTF-8");
+    String name = request.getParameter("name");
+    String pass = request.getParameter("pass");
+    User user = new User(name, pass);
+    LoginLogic loginLogic = new LoginLogic();
+    boolean isLogin = loginLogic.execute(user);
 
+    if (isLogin) {
+      HttpSession session = request.getSession();
+      session.setAttribute("loginUser", user);
+    }
+    RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/loginResult.jsp");
+    dispatcher.forward(request, response);
+  }
 }
