675dafd876d0ac86df4347652ec27c16f54aec0b
azu-wakinoza <wakinoza@gmail.com>
2025-09-29 09:44:50 +0900

    CheckStyleの警告部分を修正

--- todo_servlet/src/main/java/model/TodoItemLogic.java
+++ todo_servlet/src/main/java/model/TodoItemLogic.java
@@ -2,39 +2,39 @@ package model;
 
 import java.util.List;
 
-/**.
- * TodoItemインスタンスをショルするクラス
+/**
+ * . TodoItemインスタンスをショルするクラス
  */
 public class TodoItemLogic {
-	/**.
-	 * TodoItemインスタンスをtodoItemListに追加するメソッド
-	 *
-	 * @param todoItem TodoItemインスタンス
-	 * @param todoItemList TodoItemのList
-	 */
-	public void add(TodoItem todoItem, List<TodoItem> todoItemList) {
-		todoItemList.add(0, todoItem);
-	}
-	
-	/**.
-	 * 指定されたファイル名を持つTodoItemインスタンスの身長を更新するメソッド
-	 * @param fileName 指定するファイル名
-	 * @param todoItemList TodoItem
-	 */
-	public void updateProgress(String fileName, List<TodoItem> todoItemList) {
-		for (int i  = 0; i < todoItemList.size(); i++) {
-			TodoItem cuttentTodo = todoItemList.get(i);
-			if(cuttentTodo.getFileName().trim().equalsIgnoreCase(fileName.trim())) {
-				if (cuttentTodo.getProgress() == Progress.PENDING) {
-					cuttentTodo.setProgress(Progress.IN_PROGRESS);
-				} else if (cuttentTodo.getProgress() == Progress.IN_PROGRESS) {
-					cuttentTodo.setProgress(Progress.COMPLETED);
-				} else {
-					todoItemList.remove(i);
-				}
-				break;
-			}
-		}
-	}
+  /**
+   * . TodoItemインスタンスをtodoItemListに追加するメソッド
+   *
+   * @param todoItem TodoItemインスタンス
+   * @param todoItemList TodoItemのList
+   */
+  public void add(TodoItem todoItem, List<TodoItem> todoItemList) {
+    todoItemList.add(0, todoItem);
+  }
 
+  /**
+   * . 指定されたファイル名を持つTodoItemインスタンスの身長を更新するメソッド
+   *
+   * @param fileName 指定するファイル名
+   * @param todoItemList TodoItem
+   */
+  public void updateProgress(String fileName, List<TodoItem> todoItemList) {
+    for (int i = 0; i < todoItemList.size(); i++) {
+      TodoItem cuttentTodo = todoItemList.get(i);
+      if (cuttentTodo.getFileName().trim().equalsIgnoreCase(fileName.trim())) {
+        if (cuttentTodo.getProgress() == Progress.PENDING) {
+          cuttentTodo.setProgress(Progress.IN_PROGRESS);
+        } else if (cuttentTodo.getProgress() == Progress.IN_PROGRESS) {
+          cuttentTodo.setProgress(Progress.COMPLETED);
+        } else {
+          todoItemList.remove(i);
+        }
+        break;
+      }
+    }
+  }
 }
