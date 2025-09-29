13ee9405c2399d5b3bd44545460331919de5ad0b
azu-wakinoza <wakinoza@gmail.com>
2025-09-29 09:34:57 +0900

    checkStyleの指摘部分を変更

--- todo_servlet/src/main/java/model/LoginLogic.java
+++ todo_servlet/src/main/java/model/LoginLogic.java
@@ -1,20 +1,19 @@
 package model;
 
-/**.
- * ログイン時の認証を行うクラス
+/**
+ * . ログイン時の認証を行うクラス
  */
 public class LoginLogic {
-	
-    /**.
-     * ログイン時の認証を行うメソッド
-     * 
-     * @param user　Userクラスのインスタンス
-     * @return　　認証の可否
-     */
-	 public boolean execute(User user) {
-		if (user.getPass().equals("1234")) {
-		    return true;
-		}
-		return false;
-	 }
+  /**
+   * . ログイン時の認証を行うメソッド
+   *
+   * @param user Userクラスのインスタンス
+   * @return 認証の可否
+   */
+  public boolean execute(User user) {
+    if (user.getPass().equals("1234")) {
+      return true;
+    }
+    return false;
+  }
 }
